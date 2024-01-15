6b1c8ec522ff3ad7b6d179d8c206da2b3f5585ff
Ammar Munir <105548542+ammarmunir4567@users.noreply.github.com>
2024-01-15 21:30:33 +0500

    Updates

---
+++ precache-manifest.34bd1e1db69d64fd49c89a9c3971e0fe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5fea7f4ce37ddbcb900",
+    "url": "/demo/static/css/main.bc596ed2.chunk.css"
+  },
+  {
+    "revision": "f5fea7f4ce37ddbcb900",
+    "url": "/demo/static/js/main.f5fea7f4.chunk.js"
+  },
+  {
+    "revision": "9ad960da41216fce2577",
+    "url": "/demo/static/js/1.9ad960da.chunk.js"
+  },
+  {
+    "revision": "4018e284924ea3ec6f9e",
+    "url": "/demo/static/css/2.f6992339.chunk.css"
+  },
+  {
+    "revision": "4018e284924ea3ec6f9e",
+    "url": "/demo/static/js/2.4018e284.chunk.js"
+  },
+  {
+    "revision": "a5b45e696fbdc703c6cc",
+    "url": "/demo/static/js/runtime~main.a5b45e69.js"
+  },
+  {
+    "revision": "6cf692eb5ac45d583c2bc080f76ee988",
+    "url": "/demo/static/media/web.6cf692eb.jpg"
+  },
+  {
+    "revision": "36c33b76c89d2e85e7d68dfb7aafc464",
+    "url": "/demo/static/media/ammar.36c33b76.jpeg"
+  },
+  {
+    "revision": "4adf8a32534ef7a018bd5d86de81f40e",
+    "url": "/demo/static/media/khan.4adf8a32.jpeg"
+  },
+  {
+    "revision": "e8001388e419b9636a17bca1aded64a1",
+    "url": "/demo/static/media/crisis.e8001388.jpeg"
+  },
+  {
+    "revision": "5bd20286ad51e47acc758fedfa018a31",
+    "url": "/demo/static/media/campus_mate.5bd20286.jpeg"
+  },
+  {
+    "revision": "0466000f18dc070700063341833bc9f9",
+    "url": "/demo/static/media/login.0466000f.jpg"
+  },
+  {
+    "revision": "163df9e70296d65a2e8476da7ef6f99f",
+    "url": "/demo/index.html"
+  }
+];
